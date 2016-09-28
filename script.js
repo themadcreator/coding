f3b652ebe52647e1e6c9f9654256f4e943b74fa4
Bill Dwyer <bdwyer@palantir.com>
2016-09-28 12:36:58 -0700

    Done editing

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("Hello!");
+var header = document.querySelector("h1");
+
+function changeColor() {
+  header.style.color = "hsl( " + Math.random() + " , 100%, 50%)";
+  setTimeout(changeColor, 500);
+}
+setTimeout(changeColor, 500);
